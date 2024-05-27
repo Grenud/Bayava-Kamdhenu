59ee66c0bbffe4d5db065d9930ba5055c7f1e044
Paramjeet singh <paramjeetsingh16346@gmail.com>
2024-05-27 20:46:20 +0530

    submmit1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,23 +6,6 @@ function App() {
   return (
     <>
     <Navbar/>
-  
-    <h1>Hello Anushka </h1>
-
-      <h2>Vaibhav</h2>
-      <h3>anurag</h3>
-
-    <h2>Vaibhav</h2>
-
-    <h3>Hello</h3>
-    
-
-    
-
-    <h3>Param</h3>
-
-    
-
     </>
   )
 }

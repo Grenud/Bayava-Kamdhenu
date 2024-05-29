8999c52357321f5a9f78d653e460ad50c1d80e16
ParamjeetDuhan <paramjeetsingh16346@gmail.com>
2024-05-29 19:53:25 +0530

    Folder Structring

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,24 +1,11 @@
-import Cow from './components/Cow/Cow.jsx';
-import Header from './components/Header/Header.jsx';
-import Main from './components/Main/Main.jsx';
-import Footer from './components/footer/footer.jsx';
-
-import Navbar from './components/navbar/navbar.jsx';
-
-
+import Home from "./components/Pages/Home"
 
 function App() {
  
 
   return (
     <div>
-    <Navbar/>
-    <Header/>
-    <Main/>
-    <Cow/>
-    <Footer/>
-    
-   
+   <Home/>
     </div>
   )
 }
